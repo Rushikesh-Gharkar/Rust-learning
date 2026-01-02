8cc7b4fd9f420a60f105e6d920cfbc0c9650468f
rus.ggs <rushikeshgharkar31@gmail.com>
2026-01-02 22:57:46 +0530

    Boolean

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    bool()
+
+}
+
+fn var(){
+    let x : i8 = 100;
+    
+    print!("X is {}" , x)
+}
+
+fn bool(){
+    let is_male = true;
+    let is_above_18 = true;
+
+    if is_male{
+        print!("You r male")
+    }
 }
